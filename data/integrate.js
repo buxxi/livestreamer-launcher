a730e93183610819f323066157bf555d20294761
BuXXi <buxxi@omfilm.net>
2015-02-04 23:28:02 +0100

    Removing the context-menu support. Displaying error messages from livestreamer as an alert. Restoring the flash player when closing VLC

--- data/integrate.js
+++ data/integrate.js
@@ -5,21 +5,38 @@ function getStreamObject() {
 		return o.clientHeight * o.clientWidth;
 	}
 
-	var objects = document.querySelectorAll("object");
+	var objects = document.querySelectorAll("object,embed,video");
 	for (var i in objects) {
 		if (!maxObject || size(maxObject) < size(objects[i])) {
 			maxObject = objects[i];
-	    	}    
+	    }    
 	}
 	return maxObject;
 }
 
-self.port.on("replaceFlashWithLoading", function() {
-	var stream = getStreamObject();
+var stream;
+var loading;
 
-	var newObj = document.createElement("div");
-	newObj.innerHTML = "Starting Livestreamer...";
-	stream.parentNode.replaceChild(newObj, stream);
+self.port.on("replaceFlashWithLoading", function(quality) {
+	stream = getStreamObject();
+
+	loading = document.createElement("div");
+	loading.innerHTML = "Starting Livestreamer with quality " + quality;
+	loading.style.fontSize = '24px';
+	loading.style.fontWeight = 'bold';
+	loading.style.background = 'black';
+	loading.style.color = 'white';
+	loading.style.textAlign = 'center';
+	stream.parentNode.replaceChild(loading, stream); 
+});
+
+self.port.on("restoreFlash", function() {
+	loading.parentNode.replaceChild(stream, loading);
+	stream = null;
+});
+
+self.port.on("showError", function(event) {
+	alert(event);
 });
 
 
--- data/menu.js
+++
@@ -1,8 +0,0 @@
-self.on("context", function(node, data) { 
-	self.postMessage("load");
-	return "Livestreamer";
-});
-
-self.on("click", function (node, data) {
-	self.postMessage(data);
-});
